d6ff5c1e6cbf03599d0c4ffa539e30eba5c64e13
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-10 12:05:54 +0530

    Drop Down Added

--- src/script.js
+++ src/script.js
@@ -35,7 +35,26 @@ var products = [{
     "os": "Windows"
 }];
 
+var filter_OS = [];
+var filter_Brand = [];
+for(var i = 0; i < products.length; i++){
+    filter_OS.push(products[i].os);
+    filter_Brand.push(products[i].brand);
+    console.log(filter_OS);
+    console.log(filter_Brand);
+}
+
+
+
+
+
 $(document).ready(function(){
+
+    
+
+    
+
+    
     var text = " <table>\
     <tr>\
     <td>ID</td>\
@@ -47,12 +66,12 @@ $(document).ready(function(){
 
     
 for(var i =0; i<products.length;i++){
-            text +='<tr id = '+products[i].id+'>\
-             <td>' + products[i].id + '</td>\
-             <td>' + products[i].name + '</td>\
-             <td>' + products[i].brand + '</td>\
-             <td>' + products[i].os + '</td>\
-             <td> <a href="#" id = "hide1" data-r_id='+products[i].id +' > X </a> </td>\
+            text +='<tr class="row" id = '+products[i].id+'>\
+             <td class="row">' + products[i].id + '</td>\
+             <td class="row">' + products[i].name + '</td>\
+             <td class="row">' + products[i].brand + '</td>\
+             <td class="row">' + products[i].os + '</td>\
+             <td class="row"> <a href="#" id = "hide1" data-r_id='+products[i].id +' > X </a> </td>\
          </tr>'
         }
         text+= '</table>'
@@ -63,5 +82,40 @@ for(var i =0; i<products.length;i++){
             $('#' + row).hide();
             
         });
+        osDrop = '<label for os ="">Filter OS</label>\
+    <select name = "os" id = "os">\
+    <option value = "none">None</option>';
+    
+    for(var item of filter_OS){
+        osDrop += '<option value = "'+item+'">' +item+ '</option>';
+    }
+    osDrop +=
+    '</select>'
+
+    
+
+    $('table').before(osDrop);
+
+    brandDrop = '<label for brand ="">Filter Brand</label>\
+    <select name = "brand" id = "brand">\
+    <option value = "none">None</option>';
+    for(var item of filter_Brand){
+        brandDrop += '<option value = "'+item+'">' +item+ '</option>';
+    }
+    brandDrop +=
+    '</select>'
+
+    
+    $('table').before(brandDrop);
+
+    $('#os').change(function(){
+        var filterValue = $(this).val();
+        var row = $('.row');
+        
+    });
+
+
+        
+
 });
 
