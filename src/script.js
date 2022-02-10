378c263ba9a1b3321ef052dd93b4e7aee3adaba3
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-10 15:59:50 +0530

    drop down hide

--- src/script.js
+++ src/script.js
@@ -39,6 +39,7 @@ var filter_OS = new Set();
 var filter_Brand = new Set();
 var  productDisplay = [];
 var productDisplay1 = [];
+
 for(var i = 0; i < products.length; i++){
     filter_OS.add(products[i].os);
     filter_Brand.add(products[i].brand);
@@ -52,32 +53,10 @@ for(var i = 0; i < products.length; i++){
 
 $(document).ready(function(){
 
-    
+        display(products);
 
     
-
     
-    var text = " <table>\
-    <tr>\
-    <td>ID</td>\
-    <td>Name</td>\
-    <td>Brand</td>\
-    <td>Operating System</td>\
-    <td>Remove</td>\
-   </tr>";
-
-    
-for(var i =0; i<products.length;i++){
-            text +='<tr  id = '+products[i].id+'>\
-             <td data-type = '+products[i].id+' class="row">' + products[i].id + '</td>\
-             <td data-type = '+products[i].name+' class="row">' + products[i].name + '</td>\
-             <td data-type = '+products[i].brand+' class="row">' + products[i].brand + '</td>\
-             <td data-type = '+products[i].os+' class="row">' + products[i].os + '</td>\
-             <td data-type = '+""+' class="row"> <a href="#" id = "hide1" data-r_id='+products[i].id +' > X </a> </td>\
-         </tr>'
-        }
-        text+= '</table>'
-        $('#container').html(text);
         $('body').on("click", '#hide1', function(){
             var row = $(this).data('r_id');
             console.log(row);
@@ -109,44 +88,97 @@ for(var i =0; i<products.length;i++){
 
     
     $('table').before(brandDrop);
-
+    
     $('#os').change(function(){
         productDisplay = []
         var filterOS = $(this).val();
         for(var i = 0; i < products.length; i++){
-            if(products[i].os == filterOS){
+            if(products[i].os == filterOS ){
                 productDisplay.push(products[i]);
 
             }else if(filterOS == 'none'){
                 productDisplay = products;
             }
         }
+        
         console.log(productDisplay);
+        display(productDisplay, osDrop, brandDrop);
 
-        $('#brand').change(function(filterOS,productDisplay){
-            productDisplay1 = []
-            var filterBrand = $(this).val();
-            for(var i = 0; i < productDisplay.length; i++){
-                if(productDisplay[i].brand == filterBrand){
-                    productDisplay1.push(productDisplay[i]);
+        
+        // $('table').html(productDisplay);
+
+
+        // $('#brand').change(function(){
+        //     productDisplay1 = []
+        //     var filterBrand = $(this).val();
+        //     for(var i = 0; i < products.length; i++){
+        //         if(products[i].brand == filterBrand){
+        //             productDisplay1.push(products[i]);
     
-                }else if(filterBrand == 'none'){
-                    productDisplay1 = productDisplay;
-                }else if(filterOS == 'none'){
-                    productDisplay1 = products;
-                }
-            }
-            console.log(productDisplay1);
+        //         }else if(filterBrand == 'none'){
+        //             productDisplay1 = products;
+        //         }
+        //     }
+        //     console.log(productDisplay1);
     
             
-        });
+        // });
+
+        
+    });
+    $('#brand').change(function(){
+        productDisplay1 = []
+        var filterBrand = $(this).val();
+        for(var i = 0; i < products.length; i++){
+            if(products[i].brand == filterBrand){
+                productDisplay1.push(products[i]);
+
+            }else if(filterBrand == 'none'){
+                productDisplay1 = products;
+            }
+        }
+        
+        console.log(productDisplay1);
+        display(productDisplay1 , osDrop, brandDrop);
 
         
     });
     
+    
 
 
+function display(resArray, osDrop, brandDrop){
+
+    var text = " <table>\
+    <tr>\
+    <td>ID</td>\
+    <td>Name</td>\
+    <td>Brand</td>\
+    <td>Operating System</td>\
+    <td>Remove</td>\
+   </tr>";
+
+    
+for(var i =0; i<resArray.length;i++){
+            text +='<tr  id = '+resArray[i].id+'>\
+             <td data-type = '+resArray[i].id+' class="row">' + resArray[i].id + '</td>\
+             <td data-type = '+resArray[i].name+' class="row">' + resArray[i].name + '</td>\
+             <td data-type = '+resArray[i].brand+' class="row">' + resArray[i].brand + '</td>\
+             <td data-type = '+resArray[i].os+' class="row">' + resArray[i].os + '</td>\
+             <td data-type = '+""+' class="row"> <a href="#" id = "hide1" data-r_id='+resArray[i].id +' > X </a> </td>\
+         </tr>'
+        }
+        text+= '</table>'
+        $('#container').html(text);
+        $('table').before(osDrop);
+        $('table').before(brandDrop);
+
+}  
         
 
 });
+
+
+
+
 
